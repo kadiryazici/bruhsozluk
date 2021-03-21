c31e30933ec0c8bbadbec1618fb4e1836c6fdfcd
kadiryazici <kyzc411@outlook.com.tr>
2021-03-21 06:27:51 +0300

    improved(readibility)

--- src/server/post/addHeader.ts
+++ src/server/post/addHeader.ts
@@ -13,35 +13,42 @@ import { Config } from '@config';
 export default defineAsyncHandler(async (req, res) => {
    const { name }: AddHeaderBody = req.body;
 
-   if (name && trimLength(name) > 0) {
-      const db = await useDB();
+   const { headerMax, headerMin } = Config.add_header;
+
+   if (name) {
       const sanitizedName = sanitizeHeaderName(name);
+      const trimmedHeaderLength = trimLength(sanitizedName);
+      if (trimmedHeaderLength >= headerMin) {
+         const db = await useDB();
 
-      const header = db
-         .get('headers')
-         .find({
-            name: sanitizedName,
-         })
-         .value();
-
-      if (!header) {
-         const id = nanoid();
-         const name = sanitizedName.slice(0, Config.add_header.headerMax);
-
-         await db
+         const header = db
             .get('headers')
-            .push({
-               entries: [],
-               id,
-               name,
+            .find({
+               name: sanitizedName,
             })
-            .write();
+            .value();
 
-         const response = { id, name } as AddHeaderResponse;
-         res.status(200).json(response);
-         // ENDIF
+         if (!header) {
+            const id = nanoid();
+            const name = sanitizedName.slice(0, headerMax);
+
+            await db
+               .get('headers')
+               .push({
+                  entries: [],
+                  id,
+                  name,
+               })
+               .write();
+
+            const response = { id, name } as AddHeaderResponse;
+            res.status(200).json(response);
+            // ENDIF
+         } else {
+            responseError(res, Msg.add_header.error.headerExists);
+         }
       } else {
-         responseError(res, Msg.add_header.error.headerExists);
+         responseError(res, Msg.add_header.error.nameNeeded);
       }
    } else {
       responseError(res, Msg.add_header.error.nameNeeded);
