648f58a9bce6cbf3598ddaf5d762b97a87c665eb
kadiryazici <kyzc411@outlook.com.tr>
2021-07-07 09:16:49 +0300

    remove unnecessary code

--- src/api/postAddEntry.ts
+++ src/api/postAddEntry.ts
@@ -4,9 +4,6 @@ import type { AddEntryBody, AddEntryResponse } from '/src/api/types.d';
 import { useAppStore } from '/src/stores/appStore';
 
 export async function postAddEntry(body: string, header_id: string) {
-   const appStore = useAppStore();
-   const [user] = appStore.userInformation;
-
    const url = ReqURL(`/add_entry`);
    return await axios.post<AddEntryResponse>(url, {
       body,
